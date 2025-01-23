b9f81aab114aa6e1bb3208fcec1f096902d4b1b4
Nilotpal Bhuyan <nilotpalbhuyan352@gmail.com>
2025-01-23 08:01:14 +0530

    Minor silly mistake rectified

--- Python Projects/chaos_user_determined.py
+++ Python Projects/chaos_user_determined.py
@@ -2,8 +2,8 @@
 # A simple program illustrating chaotic behavior.
 def main():
     print("This program illustrates chaotic behaviour.")
-    x = eval(input("Enter a number between 0 and 1: "))
     n = eval(input("How many numbers should I print? ") )
+    x = eval(input("Enter a number between 0 and 1: "))
     for i in range(n):
         x = 3.9 * x * 1-x
         print(x)
